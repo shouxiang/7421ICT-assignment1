84eef955ba8425e1274f00caab143a4be584c97f
shouxiang <shouxiang.zh@gmail.com>
2024-04-10 22:30:59 +0800

    finish the caret toggle

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,31 +1,43 @@
 import { View, Text, StyleSheet } from "react-native";
 import { todoItemColor } from "../constants/color";
+import { List } from "react-native-paper";
+import { eventIconColor } from "../constants/color";
 
 export default ({ title, description }) => (
-  <View style={styles.container}>
-    <Text style={styles.title}>{title}</Text>
-    <Text style={styles.description}>{description}</Text>
-  </View>
+  <List.Section style={styles.container}>
+    <List.Accordion
+      title={title}
+      left={() => <List.Icon color={eventIconColor} icon="calendar" />}
+      titleStyle={{ fontSize: 20, color: "black" }}
+      style={styles.accordion}
+    >
+      <View style={styles.description}>
+        <Text style={{ fontSize: 15 }}>{description}</Text>
+      </View>
+    </List.Accordion>
+    <View style={styles.buttonPanel}></View>
+  </List.Section>
 );
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: todoItemColor,
     borderRadius: 10,
-    paddingHorizontal: 10,
-    minHeight: 25,
-    justifyContent: "center",
-    marginBottom: 10,
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: "bold",
+    paddingHorizontal: 5,
+    overflow: "hidden",
     marginVertical: 5,
     // borderWidth: 1,
   },
-  description: {
-    fontSize: 16,
-    marginBottom: 5,
+  accordion: {
+    backgroundColor: todoItemColor,
+    height: 50,
+    justifyContent: "center",
+    paddingTop: 0,
     // borderWidth: 1,
   },
+  description: {
+    // borderWidth: 1,
+    marginBottom: 5,
+  },
+  buttonPanel: {},
 });
--- src/constants/color.js
+++ src/constants/color.js
@@ -4,3 +4,4 @@ export const todoItemColor = "#add8e6";
 export const activeButtonColor = "#add8e6";
 export const pressedButtonColor = "#a8b4e8";
 export const iconColor = "green";
+export const eventIconColor = "#1e00ff";
